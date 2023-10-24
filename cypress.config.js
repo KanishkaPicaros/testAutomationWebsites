0175eeb125eb251defddc472596014cc8b232a9a
SachiHD <himeshila@gmail.com>
2023-10-24 23:50:24 +0800

    config changes

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,11 @@ const { beforeRunHook, afterRunHook } = require("cypress-mochawesome-reporter/li
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
 
+  video: "true",
+  videoCompression:"10", //lower the number higher the quality. Can be set from 1 -55
+  viewportWidth:1080,
+  viewportHeight: 720,
+  
   reporterOptions: {
     charts: true,
     reportPageTitle: "custom-title",
@@ -15,6 +20,10 @@ module.exports = defineConfig({
   },
 
   fixturesFolder: "orange_hrm/fixtures",
+  screenshotsFolder: "orange_hrm/screenshots",
+  reportDir: "orange_hrm/reports",
+  videosFolder:"orange_hrm/video",
+
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
     userName: "Admin",
@@ -22,6 +31,8 @@ module.exports = defineConfig({
       "73884defd0b7bf9a01abdc8a2b341e6b048f3efb0009d025d67279a9716cdad312c9e610476bb454e7452f937322aea3IZppvoTY7c2q2PcncNnNLw==",
     specPattern: "orange_hrm/specs/*",
     supportFile: "orange_hrm/support/e2e.js",
+    
+
 
     setupNodeEvents(on) {
       require("cypress-mochawesome-reporter/plugin")(on)
